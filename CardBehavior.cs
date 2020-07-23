878b09c4f353bb3fcd7afd2d9aab425d17b6050c
EacyCoding <62992506+EacyCoding@users.noreply.github.com>
2020-07-23 13:51:36 +0200

    Card Property & Behavior
    second

---
+++ CardBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class CardBehavior : MonoBehaviour
+{
+    
+    public void OnClick()
+    {
+        Debug.Log("Click on Card.");
+        GetComponentInParent<GamePanelBehavior>().GetContext().OnClick(this);
+    }
+
+    internal void Dispose()
+    {
+        Destroy(this.gameObject);
+    }
+
+    internal void PlayAudio()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    internal void PlayAnimation()
+    {
+        gameObject.GetComponent<PlayableDirector>().Play();
+    }
+
+    public void MiddleAnimation()
+    {
+        ChangeImage();
+    }
+
+    public void EndAnimation()
+    {
+        gameObject.GetComponent<AudioSource>().Stop();
+        GetComponentInParent<GamePanelBehavior>().GetContext().AnimationEnd(this);
+    }
+
+    internal Sprite GetCurrentSprite()
+    {
+        CardProperty cardProperty = GetComponentInParent<GamePanelBehavior>().ReturnCardProperty(this.gameObject);
+        return gameObject.GetComponent<Image>().sprite;
+    }
+
+    private void ChangeImage()
+    {
+        CardProperty cardProperty = GetComponentInParent<GamePanelBehavior>().ReturnCardProperty(this.gameObject);
+        Sprite currentSprite = gameObject.GetComponent<Image>().sprite;
+        if (currentSprite == cardProperty.BackImage)
+        {
+            gameObject.GetComponent<Image>().sprite = cardProperty.FrontImage;
+        } 
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = cardProperty.BackImage;
+        }
+    }
+
+}
---
+++ CardProperty.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardProperty
+{
+    internal Sprite BackImage;
+    internal Sprite FrontImage;
+    internal GameObject Instance;
+
+
+    public CardProperty(GameObject instance, Sprite backimage, Sprite frontimage)
+    {
+        this.BackImage = backimage;
+        this.FrontImage = frontimage;
+        this.Instance = instance;
+    }
+
+    public override string ToString()
+    {
+        return $"InstanceID: {Instance.GetInstanceID()}, Backimage: {BackImage.name}, Frontimage: {FrontImage.name}";
+    }
+}
